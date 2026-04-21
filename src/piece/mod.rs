4513fa1f449831388f9fd6b51373567157de2bf8
Kieran Llarena <baymax709@gmail.com>
2026-04-21 19:08:40 -0400

    fix: support transparent pngs

--- src/piece/mod.rs
+++ src/piece/mod.rs
@@ -129,7 +129,7 @@ impl Piece {
                 entry_point: Some("fs_main"),
                 targets: &[Some(wgpu::ColorTargetState {
                     format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
+                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),
                     write_mask: wgpu::ColorWrites::ALL,
                 })],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
